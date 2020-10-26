4337c8272ee0ef64a9553e4423eb00e9f272a8e4
Mincan Yang <49733113+ymcmatt@users.noreply.github.com>
2020-10-25 22:45:10 -0400

    basic option UI added
    basic option UI added

---
+++ Assets/Scripts/StoryBranch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryBranch : MonoBehaviour
+{
+
+    public void Option1()
+    {
+
+    }
+
+    public void Option2()
+    {
+
+    }
+}
