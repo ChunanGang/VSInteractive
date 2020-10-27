41e0860e582edab093bfc9959b113966120a39f0
Mincan Yang <49733113+ymcmatt@users.noreply.github.com>
2020-10-27 16:36:56 -0400

    basic UIs and animations

--- Assets/Scripts/FirstScene/StoryBranch.cs
+++ Assets/Scripts/FirstScene/StoryBranch.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class StoryBranch : MonoBehaviour
 {
+    public GameObject MC;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = MC.GetComponent<Animator>();
+    }
+    public void Option2()
+    {
+        animator.SetBool("isRunning", true);
+        Debug.Log("isRunning");
+        Debug.Log(animator.GetBool("isRunning"));
+    }
 
     public void Option1()
     {
-
-    }
-
-    public void Option2()
-    {
-
+        animator.SetBool("isWalking", true);
+        Debug.Log("isWalking");
+        Debug.Log(animator.GetBool("isWalking"));
     }
 }
