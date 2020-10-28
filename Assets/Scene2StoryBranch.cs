3c8ae5e315e8dae4e73a3ae0d0c00a9a07ad2ba9
ziw261 <ziw261@ucsd.edu>
2020-10-27 22:39:27 -0400

    Scene 2 Add First Choice

---
+++ Assets/Scene2StoryBranch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene2StoryBranch : MonoBehaviour
+{
+    public GameObject MC;
+    Animator animator;
+    public Slider suspicionSlider;
+    public Slider producionSlider;
+
+    private void Start()
+    {
+        animator = MC.GetComponent<Animator>();
+    }
+    public void Option2()
+    {
+        //animator.SetBool("isRunning", true);
+        Debug.Log("isRunning");
+        Debug.Log(animator.GetBool("isRunning"));
+    }
+
+    public void Option1()
+    {
+        //animator.SetBool("isWalking", true);
+        Debug.Log("isWalking");
+        Debug.Log(animator.GetBool("isWalking"));
+    }
+}
---
+++ Assets/Scripts/SecondScene/Scene2Controller.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene2Controller : MonoBehaviour
+{
+
+    public GameObject storyUI;
+    public GameObject optionUI;
+    public Text optionUIBox1Text;
+    public Text optionUIBox2Text;
+    public GameObject statusUI;
+    public float cameraMoveTime1;
+    public float autoNextLineTime;
+    public Text storyText;
+
+    private int nextLine = 0;
+    private List<string> story = new List<string>();
+
+
+    private void Start()
+    {
+        GenerateStoryBlock1();
+        storyUI.SetActive(true);
+        statusUI.SetActive(false);
+        optionUI.SetActive(false);
+        storyText.fontStyle = FontStyle.BoldAndItalic;
+        storyText.text = story[nextLine++].ToString();
+        StartCoroutine(WaitForCamMoveAndPrintText(cameraMoveTime1));
+
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        /*try
+        {
+            if (nextLine == 12)
+            {
+                storyUI.SetActive(false);
+                optionUI.SetActive(true);
+            }
+            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+            {
+                storyText.text = story[nextLine].ToString();
+                nextLine += 1;
+            }
+        }
+        catch
+        {
+        }*/
+
+    }
+
+    private void GenerateStoryBlock1()
+    {
+        story.Add("White House, before press conference.");
+        story.Add("There must be some juciy news somewhere.");
+    }
+
+
+    IEnumerator WaitForCamMoveAndPrintText(float cameraMoveTime)
+    {
+        yield return new WaitForSeconds(cameraMoveTime);
+        storyText.fontStyle = FontStyle.Normal;
+        storyText.text = story[nextLine++].ToString();
+        yield return new WaitForSeconds(autoNextLineTime);
+        storyUI.SetActive(false);
+        SetUpFirstChoice();
+        optionUI.SetActive(true);
+
+    }
+
+
+    private void SetUpFirstChoice()
+    {
+        optionUIBox1Text.text = "Staying";
+        optionUIBox2Text.text = "Sneaking";
+    }
+
+}
