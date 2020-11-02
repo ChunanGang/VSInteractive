8002c11d2bfd02c3040f2b224cd69b9bf42b5c6f
ChunanGang <chunangang@gmail.com>
2020-11-01 23:52:02 -0500

    add static class to keep game status

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameStatus
+{
+    private static int suspiciousValue;
+    private static int productivity;
+    public const int suspiciousValueMax = 100;
+    public const int productivityMax = 100;
+
+    public static int getSuspiciousValue()
+    {
+        return suspiciousValue;
+    }
+
+    public static int getProductivity()
+    {
+        return productivity;
+    }
+
+    public static void incrSuspicious(int incrAmout)
+    {
+        suspiciousValue += incrAmout;
+        suspiciousValue = Mathf.Clamp(suspiciousValue, 0, suspiciousValueMax);
+    }
+
+    public static void incrProductivity(int incrAmout)
+    {
+        productivity += incrAmout;
+        productivity = Mathf.Clamp(productivity, 0, productivityMax);
+    }
+}
--- Assets/Scripts/Tennis/CanvasController.cs
+++ Assets/Scripts/Tennis/CanvasController.cs
@@ -2,47 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Playables;
 
 public class CanvasController : MonoBehaviour
 {
     public GameObject storyUI;
     public GameObject optionUI;
     public Text storyText;
+    private int state = 0;
 
     private int nextLine = 0;
-    private List<string> story = new List<string>();
+    private List<string> startConversation = new List<string>();
+    private List<string> gamePointConversation = new List<string>();
 
+    // timeline
+    public PlayableDirector startingTimeline;
 
     private void Start()
     {
-        story.Add("Man I forgot to set up an alarm last night");
-        story.Add("Now I am going to be late for work");
-        story.Add("Boss gonna be mad");
-        story.Add("plus boss is not happy with my work long time ago");
-        story.Add("I really need to get to work on time");
-        story.Add("which road should I pick");
+        storyUI.SetActive(false);
+        optionUI.SetActive(false);
+        startConversation.Add("BOSS: \"Man let's beat them!\"");
+        startConversation.Add("\"...\"" );
+        startConversation.Add("\"OK I will try my best!\"");
+        startConversation.Add("BOSS: \"You better don't disappoint me.\"");
 
+        gamePointConversation.Add("BOSS: \"Ok it's game point now!\"");
+        gamePointConversation.Add("BOSS: \"One more to go.\"");
+        gamePointConversation.Add("BOSS: \"We can win this !\"");
+        gamePointConversation.Add("\"Umm.. Should I use my super power to win this?\"");
+        gamePointConversation.Add("BOSS: \"What were you mumbling ?\"");
+        gamePointConversation.Add("\"Nothing. Let's get it.\"");
     }
 
     // Update is called once per frame
     void Update()
     {
-        try
+        switch (state)
         {
-            if (nextLine == 6)
-            {
-                storyUI.SetActive(false);
-                optionUI.SetActive(true);
-            }
-            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
-            {
-                storyText.text = story[nextLine].ToString();
-                nextLine += 1;
-            }
-        }
-        catch
-        {
+            // starting conversation with boss
+            case 1:
+                storyText.text = startConversation[nextLine].ToString();
+                if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+                    nextLine += 1;
+                if (nextLine > startConversation.Count - 1)
+                {
+                    nextLine = 0;
+                    storyUI.SetActive(false);
+                    startingTimeline.Play();
+                    state += 1;
+                }
+                break;
+            // playing tennis game 1
+            case 2:
+                break;
+            // game point conversation
+            case 3:
+                storyText.text = gamePointConversation[nextLine].ToString();
+                if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+                    nextLine += 1;
+                if (nextLine > gamePointConversation.Count - 1)
+                {
+                    nextLine = 0;
+                    storyUI.SetActive(false);
+                    startingTimeline.Play();
+                    state += 1;
+                }
+                break;
+            // playing tennis game 2
+            case 4:
+                break;
         }
 
+    }
+
+    public void triggerStartConversation()
+    {
+        startingTimeline.Pause();
+        storyUI.SetActive(true);
+        state = 1;
+    }
+
+    public void triggerGamePointConversation()
+    {
+        //print("--");
+        startingTimeline.Pause();
+        storyUI.SetActive(true);
+        state = 3;
     }
 }
